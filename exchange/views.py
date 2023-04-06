8e5aee1b79e050083dba7919f7ee817bbd161dd1
bogdan.chayka <bogdan.chayka@btc-trade.com.ua>
2023-04-06 23:49:55 +0300

    fix a little

--- exchange/views.py
+++ exchange/views.py
@@ -4,22 +4,25 @@ from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import render
 import requests
+from fintex.common import json_true, json_500false
+import traceback
 
 from .models import rate, Orders
+
 
 def main(req):
     return render(req, "exchange/detail.html", {})
 
-#TODO protect this from spam
-def get_rate(req, from_c, to_c):
 
+# TODO  protect this from spam
+def get_rate(req, from_c, to_c):
     try:
-        res = requests.get("https://btc-trade.com.ua/ticker/")
-        if res.status_code !=200:
+        res = requests.get("https://btc-trade.com.ua/ticker/%s_%s" % (from_c.lower(), to_c.lower()), )
+        if res.status_code != 200:
             raise Exception("not avaliable")
 
         resj = res.json()
-
-        return
+        return json_true(req, {"result": {"rate": resj["sell"]}})
     except:
-        return
+        traceback.print_exc()
+        return json_500false(req, {"description": "not avalible"})
