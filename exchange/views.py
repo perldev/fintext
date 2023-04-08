5cc11a9f0958d75c70d7e3c6867dc1a864c708be
Andrey Grand <grand.edocs@gmail.com>
2023-04-08 10:48:19 +0300

    added expire field into API response

--- exchange/views.py
+++ exchange/views.py
@@ -9,6 +9,9 @@ import traceback
 
 from .models import rate, Orders
 
+from datetime import datetime
+from datetime import timedelta
+
 
 def main(req):
     return render(req, "main.html", {})
@@ -17,13 +20,15 @@ def main(req):
 # TODO  protect this from spam
 def get_rate(req, currency_from, currency_to):
     try:
+        current_time = datetime.now()
+        expire_time = current_time + timedelta(minutes=15)
         currency_pair = currency_from.lower() + '_' + currency_to.lower()
         res = requests.get("https://btc-trade.com.ua/api/ticker/%s_%s" % (currency_from.lower(), currency_to.lower()), )
         if res.status_code != 200:
             raise Exception("not avaliable")
 
         resj = res.json()
-        return json_true(req, {"result": {"rate": resj[currency_pair]["sell"]}})
+        return json_true(req, {"result": {"rate": resj[currency_pair]["sell"], "expire": expire_time}})
     except:
         traceback.print_exc()
         return json_500false(req, {"description": "not avaliable"})
