9cd0d10654ce90883aca0736ada5eb2f86c016e0
bogdan.chayka <bogdan.chayka@btc-trade.com.ua>
2023-04-11 16:06:36 +0300

    fix typo

--- fintex/wsgi.py
+++ fintex/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import traceback()
+import traceback
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fintex.settings')
 import bitstamp.client
 from exchange.models import rate, Currency
