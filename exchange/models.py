e5ff6c7879be36c49514405f651285d8ad41aeed
Andrey Grand <grand.edocs@gmail.com>
2023-04-19 18:59:11 +0300

    create trans model

--- exchange/models.py
+++ exchange/models.py
@@ -235,3 +235,45 @@ class FiatAccounts(models.Model):
     
     def __str__(self):
         return self.card_number
+    
+
+class trans(models.Model):
+    account = models.CharField(verbose_name="account ",
+                               max_length=255,
+                               editable=False,
+                               null=False,
+                               blank=False)
+    payment_id = models.CharField(verbose_name="payment id",
+                                  max_length=255,
+                                  editable=False,
+                                  null=False,
+                                  blank=False)
+    status = models.CharField(max_length=40, choices=STATUS_ORDER, default='created', verbose_name="Статус")
+    currency = models.ForeignKey("exchange.Currency",
+                                 verbose_name="Currency",
+                                 on_delete=models.PROTECT,
+                                 related_name="currency_of_trans", )
+    pub_date = models.DateTimeField(default=datetime.now,
+                                    verbose_name="Дата публикации")
+    processed_date = models.DateTimeField(auto_now=False,
+                                          verbose_name="Дата валидности",
+                                          editable=True,
+                                          null=True,
+                                          blank=True)
+    amnt = models.DecimalField(decimal_places=20,
+                               max_digits=40,
+                               verbose_name="amnt give",
+                               max_length=255, editable=True)
+    operator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Опертор",
+                                related_name="user_issued",
+                                editable=False, on_delete=models.PROTECT,
+                                null=True,
+                                blank=True)
+    txid = models.CharField(verbose_name="crypto txid", null=True, blank=True, max_length=255)
+
+    class Meta:
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
+
+    def unicode(o):
+        return str(o.id) + " " + str(o.pub_date) + " " + o.currency + " " + o.amnt
--- oper/models.py
+++ oper/models.py
@@ -3,42 +3,6 @@ from datetime import datetime
 from fintex import settings
 from exchange.models import Currency, STATUS_ORDER, Orders
 
-# Create your models here.
-class crypto_trans(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Клиент",
-                             related_name="user_oper",
-                             editable=False, on_delete=models.PROTECT,
-                             null=True,
-                             blank=True)
-    status = models.CharField(max_length=40, choices=STATUS_ORDER, default='created', verbose_name="Статус")
-
-    currency = models.ForeignKey("exchange.Currency", verbose_name="Currency",
-                                      on_delete=models.PROTECT,
-                                      related_name="currency_of_trans", )
-
-    pub_date = models.DateTimeField(default=datetime.now, verbose_name="Дата публикации")
-
-    processed_date = models.DateTimeField(auto_now=False, verbose_name="Дата валидности", editable=True, null=True,
-                                          blank=True)
-
-    amnt = models.DecimalField(decimal_places=20, max_digits=40, verbose_name="amnt give",
-                               max_length=255, editable=True)
-
-    operator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Опертор",
-                                related_name="user_issued",
-                                editable=False, on_delete=models.PROTECT,
-                                null=True,
-                                blank=True)
-
-    crypto_txid = models.CharField(verbose_name="crypto txid", null=True, blank=True, max_length=255)
-
-    class Meta:
-        verbose_name = 'крипто транзакция'
-        verbose_name_plural = 'крипто транзакции'
-
-    def __unicode__(o):
-        return str(o.id) + " " + str(o.pub_date) + " " + o.currency + " " + o.amnt
-
 
 class chat(models.Model):
     deal = models.ForeignKey("exchange.Orders",
