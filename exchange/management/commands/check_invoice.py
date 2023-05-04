988a271eb42ef778f08feafa07440b4ca41ac214
bogdan.chayka <bogdan.chayka@btc-trade.com.ua>
2023-05-04 13:31:26 +0300

    Revert "Revert "fix""
    This reverts commit 93de396d08c824f2b9238870a31cb9ad93ea9706.

--- exchange/management/commands/check_invoice.py
+++ exchange/management/commands/check_invoice.py
@@ -4,6 +4,7 @@ from exchange.models import Invoice, CHECKOUT_STATUS_FREE
 from fintex.settings import FIAT_CURRENCIES
 from sdk.factory import CryptoFactory
 from datetime import datetime
+from decimal import Decimal
 
 class Command(BaseCommand):
     help = "Check BTC invoices"
@@ -14,20 +15,27 @@ class Command(BaseCommand):
         for i in active_invoices:
             if i.currency.title not in FIAT_CURRENCIES:
                 factory = CryptoFactory(i.currency.title)
-                actual_sum, transes = factory.get_sum_from(str(i.crypto_payments_details), i.block_height)
+                new_balance = factory.get_balance(i.crypto_payments_details.address)
 
-                # data_from_api = get_in_trans_from(str(i.crypto_payments_details), i.block_height)
-                # TODO TO FACTORY
-                if i.sum >= actual_sum:
-                    i.status = 'paid'
-                    i.save()
-                    i.crypto_payments_details.status = CHECKOUT_STATUS_FREE
-                    i.crypto_payments_details.save()
+                # tricky, because we should remember this during sweeping to the cold
+                # we doing checking the balance in order to avoid more weighty checking the list of transactions
+                if Decimal(new_balance) > Decimal(i.crypto_payments_details.technical_info):
 
-                if i.expire_date < nw:
-                    i.status = 'expired'
-                    i.save()
-                    i.crypto_payments_details.status = CHECKOUT_STATUS_FREE
-                    i.crypto_payments_details.save()
+                    actual_sum, transes = factory.get_sum_from(i.crypto_payments_details.address,
+                                                               i.block_height)
+
+                    # data_from_api = get_in_trans_from(str(i.crypto_payments_details), i.block_height)
+                    # TODO TO FACTORY
+                    if i.sum >= actual_sum:
+                        i.status = 'paid'
+                        i.save()
+                        i.crypto_payments_details.status = CHECKOUT_STATUS_FREE
+                        i.crypto_payments_details.save()
+
+                    if i.expire_date < nw:
+                        i.status = 'expired'
+                        i.save()
+                        i.crypto_payments_details.status = CHECKOUT_STATUS_FREE
+                        i.crypto_payments_details.save()
 
     
