1ca01cb33ea1bcc16c07ed36f1c143f613543fe3
bogdan.chayka <bogdan.chayka@btc-trade.com.ua>
2023-05-08 14:54:25 +0300

    fd

--- sdk/factory.py
+++ sdk/factory.py
@@ -5,10 +5,21 @@ from sdk.btc import get_current_height, get_out_trans_from, get_in_trans_from, g
 from sdk.eth import get_current_height as heth, get_out_trans_from as outeth, get_in_trans_from as ineth, \
     get_sum_from as sumfrometh, get_balance as balanceeth
 
+
+from sdk.erc20 import get_current_height as herc, get_out_trans_from as outerc, get_in_trans_from as inerc, \
+    get_sum_from as sumfromerc, get_balance as balanceerc
+
+
+
+from sdk.tron import get_current_height as htron, get_out_trans_from as outtron, get_in_trans_from as intron, \
+    get_sum_from as sumfromtron, get_balance as balancetron
+
 class CryptoFactory:
 
     def __init__(self, arg):
         self.__currency = arg
+
+
         self.__dict_call = {
             "btc":{
                 "get_current_height": get_current_height,
@@ -27,11 +38,11 @@ class CryptoFactory:
 
             },
             "usdt": {
-                "get_current_height": heth,
-                "get_out_trans_from": outeth,
-                "get_in_trans_from": ineth,
-                "get_sum_from": sumfrometh,
-                "get_balance": balanceeth
+                "get_current_height": herc,
+                "get_out_trans_from": outerc,
+                "get_in_trans_from": inerc,
+                "get_sum_from": sumfromerc,
+                "get_balance": balanceerc
 
             },
         }
